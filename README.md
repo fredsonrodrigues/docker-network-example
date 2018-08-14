345d4bf84e14083ea555481f80a603ec6977db67
Fredson Rodrigues <fredson.rodrigues.principal@gmail.com>
2018-08-14 17:05:40 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Docker network example
+
+Nesse projeto, criamos uma api que possui duas instâncias, sendo controladas por um load-ballance nginx.
---
+++ api/index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/json');
+    res.end(`{
+        name: 'fredson',
+        age: 25,
+        job: 'Developer'
+    }`);
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
