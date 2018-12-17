3bd393d09850598f63c4a8c8275ce8f8bba4633c
Fredson Rodrigues <fredson.jorge@ceuma.br>
2018-12-17 14:38:45 -0300

    Fixing bugs

--- api/index.js
+++ api/index.js
@@ -6,13 +6,15 @@ const port = 8080;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/json');
-    res.end(`{
+    res.end(`
+    {
         name: 'fredson',
         age: 25,
-        job: 'Developer'
+        job: 'Developer',
+        server: ${process.env.SERVER}
     }`);
 });
 
-server.listen(port, hostname, () => {
+server.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
